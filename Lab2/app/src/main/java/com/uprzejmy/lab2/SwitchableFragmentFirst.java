80f821a3b83f1c2b7d66088cb8c46af3121d1f2e
Uprzejmy <lmerynda@gmail.com>
2017-11-12 03:07:03 +0100

    lab2-zad3,zad4

--- Lab2/app/src/main/java/com/uprzejmy/lab2/SwitchableFragmentFirst.java
+++ Lab2/app/src/main/java/com/uprzejmy/lab2/SwitchableFragmentFirst.java
@@ -1,14 +1,16 @@
 package com.uprzejmy.lab2;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class SwitchableFragmentFirst extends Fragment
 {
-
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -18,7 +20,37 @@ public class SwitchableFragmentFirst extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_switchable_first, container, false);
+        View view = inflater.inflate(R.layout.fragment_switchable_first, container, false);
+
+        readPreferences(view);
+
+        return view;
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+
+        savePreferences();
+    }
+
+    private void readPreferences(View view)
+    {
+        SharedPreferences preferences = getActivity().getPreferences(Context.MODE_PRIVATE);
+        EditText form = view.findViewById(R.id.form_name);
+
+        form.setText(preferences.getString("name", "Name"));
+    }
+
+    private void savePreferences()
+    {
+        SharedPreferences preferences = getActivity().getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        EditText form = getActivity().findViewById(R.id.form_name);
+
+        editor.putString("name", form.getText().toString());
+        editor.commit();
     }
 }
--- Lab2/app/src/main/java/com/uprzejmy/lab2/SwitchableFragmentSecond.java
+++ Lab2/app/src/main/java/com/uprzejmy/lab2/SwitchableFragmentSecond.java
@@ -1,14 +1,16 @@
 package com.uprzejmy.lab2;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class SwitchableFragmentSecond extends Fragment
 {
-
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -18,7 +20,38 @@ public class SwitchableFragmentSecond extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_switchable_second, container, false);
+        View view = inflater.inflate(R.layout.fragment_switchable_second, container, false);
+
+        readPreferences(view);
+
+        return view;
     }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+
+        savePreferences();
+    }
+
+    private void readPreferences(View view)
+    {
+        SharedPreferences preferences = getActivity().getPreferences(Context.MODE_PRIVATE);
+        EditText form = view.findViewById(R.id.form_surname);
+
+        form.setText(preferences.getString("surname", "Surname"));
+    }
+
+    private void savePreferences()
+    {
+        SharedPreferences preferences = getActivity().getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        EditText form = getActivity().findViewById(R.id.form_surname);
+
+        editor.putString("surname", form.getText().toString());
+        editor.commit();
+    }
+
 }
